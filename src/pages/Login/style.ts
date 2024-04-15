ada7afd8f6936e11769f6da1edb72104125a6245
Yena Baek <yena2324@gmail.com>
2024-04-15 14:11:20 +0900

    style. 로그인, 회원가입 ui 구성

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -7,6 +7,15 @@ export const LoginWrapper = styled.div`
   flex-wrap: wrap;
   align-content: center;
   text-align: center;
+  font-size: 17pt;
+  color: #888888;
+  padding: 80px;
+
+  .LoginTitle {
+    font-size: 25pt;
+    margin-bottom: 30px;
+    color: black;
+  }
 
   .LoginContents {
     padding: 70px;
@@ -15,12 +24,88 @@ export const LoginWrapper = styled.div`
   .LoginBtnFrame {
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    gap: 20px;
+    padding: 30px;
   }
+
   .LoginButton {
+    font-size: 20pt;
     border-radius: 10px;
     width: 465px;
     height: 85px;
     background-color: white;
   }
+`;
+export const LoginDescription = styled.div`
+  font-size: 20pt;
+  margin-bottom: 20px;
+`;
+
+export const LoginMaintain = styled.div`
+  display: flex;
+  gap: 10px;
+  font-size: 17pt;
+`;
+
+export const OtherMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid #d2d2d2;
+  width: 525px;
+  margin-top: 20px;
+  padding-top: 35px;
+`;
+
+export const EmailNPasswordFrame = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  color: black;
+
+  .LoginIput {
+    width: 450px;
+    height: 75px;
+    border: 1px solid #d2d2d2;
+    border-radius: 10px;
+    padding-left: 80px;
+    font-size: 20pt;
+  }
+
+  .LoginIcon {
+    position: absolute;
+    left: 25px;
+  }
+`;
+const BtnStyle = `
+width: 450px;
+height: 75px;
+font-size: 20pt;
+background-color: white;
+border: 1px solid #015ffa;
+border-radius: 10px;
+`;
+export const LoginBtn = styled.button`
+  ${BtnStyle}
+`;
+
+export const PasswordFindBtn = styled.button`
+  ${BtnStyle}
+  margin-top: 35px;
+`;
+
+export const InputTitle = styled.div`
+  font-size: 20pt;
+  color: black;
+`;
+export const InputFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+  padding: 30px;
+`;
+export const InputDescriptionFrame = styled.div`
+  display: flex;
+  padding-left: 29px;
+  gap: 105px;
 `;
---
+++ src/pages/Scheduler/Scheduler.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scheduler = () => {
+  return (
+    <div>
+      <div>스케줄러~</div>
+    </div>
+  );
+};
+
+export default Scheduler;
